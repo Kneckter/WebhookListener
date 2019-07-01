cb9c195f33cf9ed4a80a2f668cad0d77296c0e34
Kasmar <kasmar@gitlab.com>
2019-06-30 20:00:05 -0400

    Updated for more protos

--- webhook/app.py
+++ webhook/app.py
@@ -13,30 +13,18 @@ from base64 import b64decode
 from webhook.utils import get_args
 
 from google.protobuf.json_format import MessageToJson
+from webhook.protos.pogoprotos.networking.responses.get_player_response_pb2 import GetPlayerResponse
+from webhook.protos.pogoprotos.networking.responses.get_holo_inventory_response_pb2 import GetHoloInventoryResponse
 from webhook.protos.pogoprotos.networking.responses.fort_search_response_pb2 import FortSearchResponse
 from webhook.protos.pogoprotos.networking.responses.encounter_response_pb2 import EncounterResponse
-from webhook.protos.pogoprotos.networking.responses.get_map_objects_response_pb2 import GetMapObjectsResponse, _GETMAPOBJECTSRESPONSE_TIMEOFDAY
-from webhook.protos.pogoprotos.networking.responses.gym_get_info_response_pb2 import GymGetInfoResponse
+from webhook.protos.pogoprotos.networking.responses.catch_pokemon_response_pb2 import CatchPokemonResponse
 from webhook.protos.pogoprotos.networking.responses.fort_details_response_pb2 import FortDetailsResponse
-from webhook.protos.pogoprotos.networking.responses.get_player_response_pb2 import GetPlayerResponse
-from webhook.protos.pogoprotos.map.weather.display_weather_pb2 import _DISPLAYWEATHER_DISPLAYLEVEL
-from webhook.protos.pogoprotos.map.weather.gameplay_weather_pb2 import _GAMEPLAYWEATHER_WEATHERCONDITION
-from webhook.protos.pogoprotos.map.weather.weather_alert_pb2 import _WEATHERALERT_SEVERITY
-
-from webhook.protos.pogoprotos.enums.team_color_pb2 import _TEAMCOLOR
-from webhook.protos.pogoprotos.enums.pokemon_id_pb2 import _POKEMONID
-from webhook.protos.pogoprotos.enums.pokemon_move_pb2 import _POKEMONMOVE
-from webhook.protos.pogoprotos.enums.raid_level_pb2 import _RAIDLEVEL
-from webhook.protos.pogoprotos.enums.gender_pb2 import _GENDER
-from webhook.protos.pogoprotos.enums.form_pb2 import _FORM
-from webhook.protos.pogoprotos.enums.costume_pb2 import _COSTUME
-from webhook.protos.pogoprotos.enums.weather_condition_pb2 import _WEATHERCONDITION
-from webhook.protos.pogoprotos.enums.quest_type_pb2 import _QUESTTYPE
-from webhook.protos.pogoprotos.data.quests.quest_reward_pb2 import _QUESTREWARD_TYPE
-from webhook.protos.pogoprotos.inventory.item.item_id_pb2 import _ITEMID
-from webhook.protos.pogoprotos.data.quests.quest_condition_pb2 import _QUESTCONDITION_CONDITIONTYPE
-from webhook.protos.pogoprotos.enums.pokemon_type_pb2 import _POKEMONTYPE
-from webhook.protos.pogoprotos.enums.activity_type_pb2 import _ACTIVITYTYPE
+from webhook.protos.pogoprotos.networking.responses.get_map_objects_response_pb2 import GetMapObjectsResponse
+from webhook.protos.pogoprotos.networking.responses.recycle_inventory_item_response_pb2 import RecycleInventoryItemResponse
+from webhook.protos.pogoprotos.networking.responses.disk_encounter_response_pb2 import DiskEncounterResponse
+from webhook.protos.pogoprotos.networking.responses.gym_get_info_response_pb2 import GymGetInfoResponse
+from webhook.protos.pogoprotos.networking.responses.get_raid_details_response_pb2 import GetRaidDetailsResponse
+from webhook.protos.pogoprotos.networking.responses.remove_quest_response_pb2 import RemoveQuestResponse
 
 log = logging.getLogger(__name__)
 compress = Compress()
@@ -50,22 +38,23 @@ class Webhook(Flask):
 
         # Routes
         self.json_encoder = CustomJSONEncoder
-        self.route("/webhook", methods=['GET', 'POST'])(self.webhook)
+        self.route("/", methods=['GET', 'POST', 'CONNECT'])(self.webhook)
+        self.route("/webhook", methods=['GET', 'POST', 'CONNECT'])(self.webhook)
 
     def webhook(self):
         args = get_args()
         
         if request.method == "GET":
             request_json = request.args
-        else:
-            request_json = request.get_json()
+        elif request.method == "POST":
+            request_json = request.get_json(request.get_data())
 
         if args.log_webhook:
             log.info('Full webhook: ' + (json.dumps(request_json, sort_keys=True)))
 
         if "protos" in request_json:
             protos = request_json.get('protos')
-        else:
+        elif "contents" in request_json:
             protos = request_json.get('contents')
             
         if "trainerlvl" in request_json:
@@ -86,27 +75,6 @@ class Webhook(Flask):
         for proto in protos_dict:
             if "method" in proto:
                 method = proto['method']
-
-            if "GetMapObjects" in proto or method == 106:
-                if "GetMapObjects" in proto:
-                    if args.log_gmo_enc:
-                        log.info('Encoded GetMapObjects: ' + proto['GetMapObjects'])
-                    gmo_response_string = b64decode(proto['GetMapObjects'])
-                elif method == 106:
-                    if args.log_gmo_enc:
-                        log.info('Encoded GetMapObjects: ' + proto['data'])
-                    gmo_response_string = b64decode(proto['data'])
-
-                gmo = GetMapObjectsResponse()
-
-                try:
-                    gmo.ParseFromString(gmo_response_string)
-                    gmo_response_json = json.loads(MessageToJson(gmo))
-                except:
-                    continue
-
-                if args.log_gmo_par:
-                    log.info('Parsed GetMapObjects: ' + (json.dumps(gmo_response_json, sort_keys=True)))
 
             if "GetPlayerResponse" in proto or method == 2:
                 if "GetPlayerResponse" in proto:
@@ -129,60 +97,36 @@ class Webhook(Flask):
                 if args.log_gpr_par:
                     log.info('Parsed GetPlayerResponse: ' + (json.dumps(get_player_response_json, sort_keys=True)))
 
-            if "GymGetInfoResponse" in proto or method == 156:
-                if "GymGetInfoResponse" in proto:
-                    if args.log_ggir_enc:
-                        log.info('Encoded GymGetInfoResponse: ' + proto['GymGetInfoResponse'])
-                    gym_get_info_response_string = b64decode(proto['GymGetInfoResponse'])
-                elif method == 156:
-                    if args.log_ggir_enc:
-                        log.info('Encoded GymGetInfoResponse: ' + proto['data'])
-                    gym_get_info_response_string = b64decode(proto['data'])
+            if "GetHoloInventoryResponse" in proto or method == 4:
+                if "GetHoloInventoryResponse" in proto:
+                    if args.log_ghir_enc:
+                        log.info('Encoded GetHoloInventoryResponse: ' + proto['GetHoloInventoryResponse'])
+                    get_holo_inventory_response_string = b64decode(proto['GetHoloInventoryResponse'])
+                elif method == 4:
+                    if args.log_ghir_enc:
+                        log.info('Encoded GetHoloInventoryResponse: ' + proto['data'])
+                    get_holo_inventory_response_string = b64decode(proto['data'])
 
-
-                ggir = GymGetInfoResponse()
+                ghir = GetHoloInventoryResponse()
 
                 try:
-                    ggir.ParseFromString(gym_get_info_response_string)
-                    gym_get_info_response_json = json.loads(MessageToJson(ggir))
+                    ghir.ParseFromString(get_holo_inventory_response_string)
+                    get_holo_inventory_response_json = json.loads(MessageToJson(ghir))
                 except:
                     continue
 
-                if args.log_ggir_par:
-                    log.info('Parsed GymGetInfoResponse: ' + (json.dumps(gym_get_info_response_json, sort_keys=True)))
-
-            if "FortDetailsResponse" in proto or method == 104:
-                if "FortDetailsResponse" in proto:
-                    if args.log_fdr_enc:
-                        log.info('Encoded FortDetailsResponse: ' + proto['FortDetailsResponse'])
-                    fort_details_response_string = b64decode(proto['FortDetailsResponse'])
-                elif method == 104:
-                    if args.log_fdr_enc:
-                        log.info('Encoded FortDetailsResponse: ' + proto['data'])
-                    fort_details_response_string = b64decode(proto['data'])
-
-
-                fdr = FortDetailsResponse()
-
-                try:
-                    fdr.ParseFromString(fort_details_response_string)
-                    fort_details_response_json = json.loads(MessageToJson(fdr))
-                except:
-                    continue
-
-                if args.log_fdr_par:
-                    log.info('Parsed FortDetailsResponse: ' + (json.dumps(fort_details_response_json, sort_keys=True)))
+                if args.log_ghir_par:
+                    log.info('Parsed GetHoloInventoryResponse: ' + (json.dumps(get_holo_inventory_response_json, sort_keys=True)))
 
             if "FortSearchResponse" in proto or method == 101:
                 if "FortSearchResponse" in proto:
-                    if args.log_frs_enc:
+                    if args.log_fsr_enc:
                         log.info('Encoded FortSearchResponse: ' + proto['FortSearchResponse'])
                     fort_search_response_string = b64decode(proto['FortSearchResponse'])
                 elif method == 101:
-                    if args.log_frs_enc:
+                    if args.log_fsr_enc:
                         log.info('Encoded FortSearchResponse: ' + proto['data'])
                     fort_search_response_string = b64decode(proto['data'])
-
 
                 frs = FortSearchResponse()
 
@@ -192,10 +136,10 @@ class Webhook(Flask):
                 except:
                     continue
 
-                if args.log_frs_par:
+                if args.log_fsr_par:
                     log.info('Parsed FortSearchResponse: ' + (json.dumps(fort_search_response_json, sort_keys=True)))
 
-            if ("EncounterResponse" in proto or method == 102) and int(trainerlvl) >= 30:
+            if ("EncounterResponse" in proto or method == 102) and int(trainerlvl) >= args.trainerlvl:
                 if "EncounterResponse" in proto:
                     if args.log_encounter_enc:
                         log.info('Encoded EncounterResponse: ' + proto['EncounterResponse'])
@@ -215,6 +159,174 @@ class Webhook(Flask):
 
                 if args.log_encounter_par:
                     log.info('Parsed EncounterResponse: ' + (json.dumps(encounter_response_json, sort_keys=True)))
+
+            if "CatchPokemonResponse" in proto or method == 103:
+                if "CatchPokemonResponse" in proto:
+                    if args.log_cpr_enc:
+                        log.info('Encoded CatchPokemonResponse: ' + proto['CatchPokemonResponse'])
+                    catch_pokemon_response_string = b64decode(proto['CatchPokemonResponse'])
+                elif method == 103:
+                    if args.log_cpr_enc:
+                        log.info('Encoded CatchPokemonResponse: ' + proto['data'])
+                    catch_pokemon_response_string = b64decode(proto['data'])
+
+                cpr = CatchPokemonResponse()
+
+                try:
+                    cpr.ParseFromString(catch_pokemon_response_string)
+                    catch_pokemon_response_json = json.loads(MessageToJson(cpr))
+                except:
+                    continue
+
+                if args.log_cpr_par:
+                    log.info('Parsed CatchPokemonResponse: ' + (json.dumps(catch_pokemon_response_json, sort_keys=True)))
+
+            if "FortDetailsResponse" in proto or method == 104:
+                if "FortDetailsResponse" in proto:
+                    if args.log_fdr_enc:
+                        log.info('Encoded FortDetailsResponse: ' + proto['FortDetailsResponse'])
+                    fort_details_response_string = b64decode(proto['FortDetailsResponse'])
+                elif method == 104:
+                    if args.log_fdr_enc:
+                        log.info('Encoded FortDetailsResponse: ' + proto['data'])
+                    fort_details_response_string = b64decode(proto['data'])
+
+                fdr = FortDetailsResponse()
+
+                try:
+                    fdr.ParseFromString(fort_details_response_string)
+                    fort_details_response_json = json.loads(MessageToJson(fdr))
+                except:
+                    continue
+
+                if args.log_fdr_par:
+                    log.info('Parsed FortDetailsResponse: ' + (json.dumps(fort_details_response_json, sort_keys=True)))
+
+            if "GetMapObjects" in proto or method == 106:
+                if "GetMapObjects" in proto:
+                    if args.log_gmo_enc:
+                        log.info('Encoded GetMapObjects: ' + proto['GetMapObjects'])
+                    gmo_response_string = b64decode(proto['GetMapObjects'])
+                elif method == 106:
+                    if args.log_gmo_enc:
+                        log.info('Encoded GetMapObjects: ' + proto['data'])
+                    gmo_response_string = b64decode(proto['data'])
+
+                gmo = GetMapObjectsResponse()
+
+                try:
+                    gmo.ParseFromString(gmo_response_string)
+                    gmo_response_json = json.loads(MessageToJson(gmo))
+                except:
+                    continue
+
+                if args.log_gmo_par:
+                    log.info('Parsed GetMapObjects: ' + (json.dumps(gmo_response_json, sort_keys=True)))
+
+            if "RecycleInventoryItemResponse" in proto or method == 137:
+                if "RecycleInventoryItemResponse" in proto:
+                    if args.log_riir_enc:
+                        log.info('Encoded RecycleInventoryItemResponse: ' + proto['RecycleInventoryItemResponse'])
+                    recycle_inventory_item_response_string = b64decode(proto['RecycleInventoryItemResponse'])
+                elif method == 137:
+                    if args.log_riir_enc:
+                        log.info('Encoded RecycleInventoryItemResponse: ' + proto['data'])
+                    recycle_inventory_item_response_string = b64decode(proto['data'])
+
+                riir = RecycleInventoryItemResponse()
+
+                try:
+                    riir.ParseFromString(recycle_inventory_item_response_string)
+                    recycle_inventory_item_response_json = json.loads(MessageToJson(riir))
+                except:
+                    continue
+
+                if args.log_riir_par:
+                    log.info('Parsed RecycleInventoryItemResponse: ' + (json.dumps(recycle_inventory_item_response_json, sort_keys=True)))
+
+            if "DiskEncounterResponse" in proto or method == 145:
+                if "DiskEncounterResponse" in proto:
+                    if args.log_der_enc:
+                        log.info('Encoded DiskEncounterResponse: ' + proto['DiskEncounterResponse'])
+                    disk_encounter_response_string = b64decode(proto['DiskEncounterResponse'])
+                elif method == 145:
+                    if args.log_der_enc:
+                        log.info('Encoded DiskEncounterResponse: ' + proto['data'])
+                    disk_encounter_response_string = b64decode(proto['data'])
+
+                der = DiskEncounterResponse()
+
+                try:
+                    der.ParseFromString(disk_encounter_response_string)
+                    disk_encounter_response_json = json.loads(MessageToJson(der))
+                except:
+                    continue
+
+                if args.log_der_par:
+                    log.info('Parsed DiskEncounterResponse: ' + (json.dumps(disk_encounter_response_json, sort_keys=True)))
+
+            if "GymGetInfoResponse" in proto or method == 156:
+                if "GymGetInfoResponse" in proto:
+                    if args.log_ggir_enc:
+                        log.info('Encoded GymGetInfoResponse: ' + proto['GymGetInfoResponse'])
+                    gym_get_info_response_string = b64decode(proto['GymGetInfoResponse'])
+                elif method == 156:
+                    if args.log_ggir_enc:
+                        log.info('Encoded GymGetInfoResponse: ' + proto['data'])
+                    gym_get_info_response_string = b64decode(proto['data'])
+
+                ggir = GymGetInfoResponse()
+
+                try:
+                    ggir.ParseFromString(gym_get_info_response_string)
+                    gym_get_info_response_json = json.loads(MessageToJson(ggir))
+                except:
+                    continue
+
+                if args.log_ggir_par:
+                    log.info('Parsed GymGetInfoResponse: ' + (json.dumps(gym_get_info_response_json, sort_keys=True)))
+
+            if "GetRaidDetailsResponse" in proto or method == 163:
+                if "GetRaidDetailsResponse" in proto:
+                    if args.log_grdr_enc:
+                        log.info('Encoded GetRaidDetailsResponse: ' + proto['GetRaidDetailsResponse'])
+                    get_raid_details_response_string = b64decode(proto['GetRaidDetailsResponse'])
+                elif method == 163:
+                    if args.log_grdr_enc:
+                        log.info('Encoded GetRaidDetailsResponse: ' + proto['data'])
+                    get_raid_details_response_string = b64decode(proto['data'])
+
+                grdr = GetRaidDetailsResponse()
+
+                try:
+                    grdr.ParseFromString(get_raid_details_response_string)
+                    get_raid_details_response_json = json.loads(MessageToJson(grdr))
+                except:
+                    continue
+
+                if args.log_grdr_par:
+                    log.info('Parsed GetRaidDetailsResponse: ' + (json.dumps(get_raid_details_response_json, sort_keys=True)))
+
+            if "RemoveQuestResponse" in proto or method == 903:
+                if "RemoveQuestResponse" in proto:
+                    if args.log_rqr_enc:
+                        log.info('Encoded RemoveQuestResponse: ' + proto['RemoveQuestResponse'])
+                    remove_quest_response_string = b64decode(proto['RemoveQuestResponse'])
+                elif method == 903:
+                    if args.log_rqr_enc:
+                        log.info('Encoded RemoveQuestResponse: ' + proto['data'])
+                    remove_quest_response_string = b64decode(proto['data'])
+
+                rqr = RemoveQuestResponse()
+
+                try:
+                    rqr.ParseFromString(remove_quest_response_string)
+                    remove_quest_response_json = json.loads(MessageToJson(rqr))
+                except:
+                    continue
+
+                if args.log_rqr_par:
+                    log.info('Parsed RemoveQuestResponse: ' + (json.dumps(remove_quest_response_json, sort_keys=True)))
 
         return 'ok'
 
